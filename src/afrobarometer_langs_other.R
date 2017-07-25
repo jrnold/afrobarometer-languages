ccd2dd6604fc36a2cee2a59fa99e65c09a113951
jrnold <jeffrey.arnold@gmail.com>
2017-07-24 21:46:37 -0700

    add other languages

---
+++ src/afrobarometer_langs_other.R
@@ -0,0 +1,47 @@
+#' ---
+#' title:  Afrobarometer Languages
+#' ---
+#'
+#' Write Dataset of Afrobarometer Languages
+#'
+#'
+library("tidyverse")
+library("rprojroot")
+library("stringr")
+library("magrittr")
+library("assertthat")
+library("rlang")
+
+OUTPUT <- find_rstudio_root_file("data", "afrobarometer_langs_others.csv")
+
+INPUT <- list(
+       "r4" = list("external", "afrobarometer", "merged_r4_data.sav"),
+       "r5" = list("external", "afrobarometer", "merged-round-5-data-34-countries-2011-2013-last-update-july-2015.sav"),
+       "r6" = list("external", "afrobarometer", "merged_r6_data_2016_36countries2.sav"),
+       "countries" = list("data-raw", "afrobarometer_countries.csv")) %>%
+  map_chr(function(x) invoke(find_rstudio_root_file, x))
+
+get_afrobarometer_langs_other <- function(filename, rnd) {
+  lang_vars <- switch(rnd,
+                      r4 = c("Q3OTHER"),
+                      r5 = ,
+                      r6 = c("Q2OTHER", "Q103OTHER", "Q116OTHER")
+  )
+  haven::read_sav(filename) %>%
+    select(one_of(lang_vars), COUNTRY) %>%
+    mutate(COUNTRY = as.integer(COUNTRY)) %>%
+    mutate_at(vars(one_of(lang_vars)), funs(as.character)) %>%
+    gather(question, value, -COUNTRY) %>%
+    mutate(value = str_to_lower(value)) %>%
+    filter(value != "") %>%
+    distinct() %>%
+    arrange(question, COUNTRY, value) %>%
+    mutate(round = rnd)
+}
+
+afrobarometer_langs_other <-
+  INPUT[str_subset(names(INPUT), "^r")] %>%
+  {map2_df(., names(.), get_afrobarometer_langs_other)}
+
+afrobarometer_langs_other %>%
+  write_csv(OUTPUT, na = "")
