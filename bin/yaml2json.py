fbdffd1a77126be35f23ac785ee95205b4cf60db
jrnold <jeffrey.arnold@gmail.com>
2018-03-08 20:42:05 -0800

    misc

--- bin/yaml2json.py
+++ bin/yaml2json.py
@@ -3,14 +3,17 @@ import yaml
 import json
 import sys
 
+
 def yaml2json(src, dst):
     with open(src, 'r') as f:
         data = yaml.load(f)
     with open(dst, 'w') as f:
-        json.dump(data, f, indent = 2)
+        json.dump(data, f, indent=2)
+
 
 def main():
     yaml2json(*sys.argv[1:3])
+
 
 if __name__ == "__main__":
     main()
